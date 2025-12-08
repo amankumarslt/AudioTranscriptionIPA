1e4f6f83e329e7e65027d4b14b99001e1cb87c0a
snigbug <snigdha1765@gmail.com>
2025-12-08 22:18:36 +0530

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Snigdha Banerjee on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
